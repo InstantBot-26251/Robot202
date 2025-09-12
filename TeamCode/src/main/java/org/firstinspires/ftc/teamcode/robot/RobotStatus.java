a543f96adddacd752b6264b12dc56f573b82ba04
Lakshya <lakshyakhandelwal28@gmail.com>
2025-09-12 15:06:53 -0700

    Updated 2.0.1 pedro, added follow path command and fixed stuff on drivetrain, added robotstatus, going to work on apriltags

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/RobotStatus.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/RobotStatus.java
@@ -1,4 +1,33 @@
 package org.firstinspires.ftc.teamcode.robot;
 
+import com.pedropathing.geometry.Pose;
+
 public class RobotStatus {
+
+    public enum Alliance {
+        NONE, RED, BLUE,
+    }
+
+    public enum RobotState {
+        DISABLED, AUTONOMOUS_INIT, AUTONOMOUS_ENABLED, TELEOP_INIT, TELEOP_ENABLED
+    }
+
+    public static long delayMs = 0;
+    public static Alliance alliance = Alliance.NONE;
+    public static RobotState robotState = RobotState.DISABLED;
+    public static Pose robotPose = new Pose();
+
+    public static void resetValues() {
+        delayMs = 0;
+        robotState = RobotState.DISABLED;
+        robotPose = new Pose();
+    }
+
+    public static boolean isEnabled() {
+        return robotState == RobotState.TELEOP_ENABLED || robotState == RobotState.AUTONOMOUS_ENABLED;
+    }
+
+    public static boolean isTeleop() {
+        return robotState == RobotState.TELEOP_INIT || robotState == RobotState.TELEOP_ENABLED;
+    }
 }
